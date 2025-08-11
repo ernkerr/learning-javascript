22e31dc37bd49ebbb137acf9fd23e3a2996f70c1
Erin Kerr <erin.kerr17@gmail.com>
2025-08-11 16:41:07 -0700

    Clear PalindromeTest

--- practice_tasks/palindromeTest.js
+++ practice_tasks/palindromeTest.js
@@ -1,18 +1,2 @@
 // cd practice_tasks
-// node palindrome.js
-
-function isPalindrome(word, reverseFunction) {
-  if (word === reverseFunction(word)) {
-    console.log("Is Palindrome");
-  } else {
-    console.log("Not a Palindrome");
-  }
-}
-
-isPalindrome("wow", (wordInput) => {
-  let reversedWord = "";
-  for (i = wordInput.length - 1; i >= 0; i--) {
-    reversedWord += wordInput[i];
-  }
-  return reversedWord;
-});
+// node palindromeTest.js
