b0b770897f7e92552fe5fd2dbb3f029c64498067
Erin Kerr <erin.kerr17@gmail.com>
2025-08-19 11:01:38 -0700

    Enhance reverse string implementation with follow-up questions for better understanding

--- practice_tasks/reverseString.js
+++ practice_tasks/reverseString.js
@@ -12,3 +12,31 @@ for (i = aString.length - 1; i >= 0; i--) {
 }
 
 console.log(reversedString);
+
+//
+// Follow up questions
+//
+
+// 1. Can you think of a different way to do it?
+
+// 2. How would you reverse an array?
+
+const array = ["one", "two", "three"];
+console.log("array:", array); // Expected output: "array:" Array ["one", "two", "three"]
+
+const reversed = array.reverse();
+console.log("reversed:", reversed); // Expected output: "reversed:" Array ["three", "two", "one"]
+
+// 3. Why can you not use this method for a string? (bonus/hint: why might you have to be careful with this method?)
+
+// reverse can only be used on arrays because it mutates or changes the original array
+// arrays are mutable (changable)
+// while strings are immutable (unchangeable)
+
+// bonus: is there a way to reverse the elements in an array without mutating the original array?
+const items = [1, 2, 3];
+console.log(items); // [1, 2, 3]
+
+const reversedItems = items.toReversed();
+console.log(reversedItems); // [3, 2, 1]
+console.log(items); // [1, 2, 3]
--- practice_tasks/reverseStringTest.js
+++ practice_tasks/reverseStringTest.js
@@ -2,3 +2,11 @@
 // node reverseStringTest.js
 
 let aString = "You're a wizard now Harry";
+
+// Follow up questions
+
+// 1. Can you think of a different way to do it?
+
+// 2. How would you reverse an array?
+
+// 3. Why can you not use this method for a string? (bonus/hint: why might you have to be careful with this method?)
