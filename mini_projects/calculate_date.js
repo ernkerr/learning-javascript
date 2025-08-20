2beee781a581e06d69d8e91ee6652e381fd70077
Erin Kerr <erin.kerr17@gmail.com>
2025-08-20 15:49:36 -0700

    Add calculate_date.js to display the current date in MM/DD/YYYY format

---
+++ mini_projects/calculate_date.js
@@ -0,0 +1,12 @@
+let now = new Date();
+
+console.log(`${now.getMonth() + 1}/${now.getDate()}/${now / getFullYear()}`);
+
+// get month
+// .getMonth()
+
+// get day
+// .getDate()
+
+// get year
+// .getFullYear()
