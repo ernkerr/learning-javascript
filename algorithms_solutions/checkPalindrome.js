bcaab189cb85138ec12940bb2f958ef1757e54b6
Erin Kerr <erin.kerr17@gmail.com>
2025-09-08 15:24:00 -0700

    Add recursive solution for palindrome check in checkPalindrome.js

--- algorithms_solutions/checkPalindrome.js
+++ algorithms_solutions/checkPalindrome.js
@@ -54,3 +54,21 @@ function checkPalindrome(string) {
 }
 
 console.log(checkPalindrome("wow"));
+
+//
+//
+//
+// alternative solution
+//
+//
+//
+
+// recursion
+
+function isPalindrome(str) {
+  if (str.length <= 1) return true;
+  if (str[0] !== str[str.length - 1]) return false;
+  return isPalindrome(str.slice(1, -1));
+}
+
+console.log(isPalindrome("wow"));
