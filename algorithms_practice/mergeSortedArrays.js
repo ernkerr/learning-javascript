ed014cec7b82334ed1de7978a464a142ed5ad94c
Erin Kerr <erin.kerr17@gmail.com>
2025-09-08 15:11:03 -0700

    Add mergeSortedArrays.js implementation to combine two sorted arrays

---
+++ algorithms_practice/mergeSortedArrays.js
@@ -0,0 +1,10 @@
+// node mergeSortedArrays.js
+
+// Given two sorted integer arrays arr1 and arr2, return a new array that combines both of them and is also sorted.
+arr1 = [1, 4, 7, 20];
+arr2 = [3, 5, 6];
+
+// explain the algorithm and the steps you're going to use to solve it in comments
+// then code it out!
+
+// console.log(combine(arr1, arr2));
