486f60cf8d1681a05d113c371f26d2555cf8e623
Erin Kerr <erin.kerr17@gmail.com>
2025-09-09 12:39:10 -0700

    Add initial implementation of sliding window concept in slidingWindow.js

---
+++ algorithms/slidingWindow.js
@@ -0,0 +1,13 @@
+// given an array, a subarray is a contigous section of the array
+// All the elements must be adjacent to each other in the original array and in their original order.
+// For example, with the array [1, 2, 3, 4], the subarrays (grouped by length) are:
+
+// Subarrays with length 1: [1], [2], [3], [4]
+// Subarrays with length 2: [1, 2], [2, 3], [3, 4]
+// Subarrays with length 3: [1, 2, 3], [2, 3, 4]
+// Subarrays with length 4: [1, 2, 3, 4]
+
+// A subarray can be defined by two indices, the start and end.
+// For example, with [1, 2, 3, 4], the subarray [2, 3] has a starting index of 1 and an ending index of 2.
+// Let's call the starting index the left bound and the ending index the right bound.
+// Another name for subarray in this context is "window".
